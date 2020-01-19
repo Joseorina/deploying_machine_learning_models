6f37d58cb774618e25bd5958f047dad39515f72f
Joseorina <orinangare@gmail.com>
2020-01-19 22:12:21 +0300

    [flask]

--- app.py
+++ app.py
@@ -1,12 +1,12 @@
-from flask import Flask
+from flask import Flask, request
 
 app = Flask(__name__)
 
 @app.route('/')
 def add():
-    a=10
-    b=20
-    return str(a + b)
+    a = request.args.get('a')
+    b = request.args.get('b')
+    return str(int(a) + int(b))
 
 if __name__ == '__main__':
-    app.run()
+    app.run(port=7000)

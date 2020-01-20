06504d6d7e2a72e660700aea7e9894f8f7567a07
Joseorina <orinangare@gmail.com>
2020-01-20 04:52:33 +0300

    [flask for ML]

---
+++ flask_predict_train.py
@@ -0,0 +1,4 @@
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.datasets import load_iris
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score

a98912624a0201d38cb04eba46bc3953deb4530f
Joseorina <orinangare@gmail.com>
2020-01-20 04:26:02 +0300

    [flask for ML]

--- app.py
+++ app.py
@@ -4,9 +4,13 @@ app = Flask(__name__)
 
 @app.route('/', methods=['POST','GET'])
 def add():
-    a = request.args.get('a')
-    b = request.args.get('b')
-    return str(int(a) + int(b))
+   a = request.form['a']
+   b = request.form['b']
+   return str(int(a) + int(b))
+
+def predict(house_size, house_beds):
+    prediction = ml_model.predict(house_price, house_beds)
+    return prediction
 
 if __name__ == '__main__':
     app.run(port=7000)
